3dfa829eead67777151dc2821b74ed370a98133e
andys8 <andys8@users.noreply.github.com>
2019-12-01 20:55:37 +0100

    Year 2019 Day 01

--- src/Year2019/Day01.hs
+++ src/Year2019/Day01.hs
@@ -10,28 +10,29 @@ import Common.Test
 import Data.Char
 
 
-main :: IO ()
 main = do
   input <- readFile "src/Year2019/input/01.txt"
-  -- print $ solvePartA input
-  -- print $ solvePartB input
+  print $ solvePartA input
+  print $ solvePartB input
   return ()
 
 -- Part A
 
 solvePartA :: String -> Int
-solvePartA = undefined
+solvePartA = sum . ((calculateFuel . read) <$>) . lines
+  where calculateFuel mass = (mass `div` 3) - 2
 
 
 -- Part B
 
 solvePartB :: String -> Int
-solvePartB = undefined
+solvePartB = sum . ((calculateFuelRec 0 . floor . read) <$>) . lines
+ where
+  calculateFuelRec fuel mass =
+    let fuel' = (mass `div` 3) - 2
+    in if fuel' <= 0 then fuel else calculateFuelRec (fuel + fuel') fuel'
 
 -- Test
 
 test :: IO ()
-test = testSolvePartA
-
-testSolvePartA :: IO ()
-testSolvePartA = runTests solvePartA []
+test = runTests solvePartB [("14", 2), ("1969", 966), ("100756", 50346)]
